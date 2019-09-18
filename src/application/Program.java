41db97f6941a4928a465500378c29335518ef0f5
rogborges <rogerioborg@gmail.com>
2019-09-18 17:27:54 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import java.util.Date;
 import java.util.List;
 
 import model.dao.DaoFactory;
@@ -33,8 +34,14 @@ public class Program {
 			System.out.println(obj);
 		}
 		
+		System.out.println("\n=== Test 4 - Seller insert ===");
+		Seller newSeller = new Seller(null, "Greg", "greg.gmail.com", new Date(), 4000.00, department);
+		sellerDao.insert(newSeller);
+		System.out.println("Inserted! New Id = "+ newSeller.getId());
+		}
+		
 			
 		
 	}
 
-}
+
