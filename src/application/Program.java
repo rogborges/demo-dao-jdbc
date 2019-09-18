266156322a09efdf8485c4c1008dcde39717eaa9
rogborges <rogerioborg@gmail.com>
2019-09-18 16:58:20 -0300

    findAll implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -26,6 +26,15 @@ public class Program {
 		for (Seller obj : list) {
 			System.out.println(obj);
 		}
+		
+		System.out.println("\n=== Test 3 - Seller findAll ===");
+		List<Seller> list2 = sellerDao.findAll();
+		for (Seller obj : list2) {
+			System.out.println(obj);
+		}
+		
+			
+		
 	}
 
 }
